d14a5501c126e86df3eed5c29eec74d6b325a179
Dmitriy Kuzmenko <kuzmenko.da@gmail.com>
2018-04-25 13:16:00 +0300

    start admin

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""rgs_001 URL Configuration
+"""tz_42 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
--- project/wsgi.py
+++ project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for rgs_001 project.
+WSGI config for tz_042 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rgs_001.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = get_wsgi_application()
